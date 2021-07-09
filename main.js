43cabd21fd5e9d794c566046c1cceebc1c2a0a2d
W.Carlos <wendcarlos18@hotmail.com>
2021-07-09 20:41:59 -0300

    terminei a seção depoimentos
    fiz o carrossel com SWIPER

--- main.js
+++ main.js
@@ -30,3 +30,14 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll')
     }
 })
+
+/* Depoimentos carousel swiper */
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+
+  });
